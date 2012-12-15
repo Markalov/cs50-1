7277e0cfae13be5862829986984b84912330cf82
Evan Sendra (dorm room) <evan@evansendra.com>
2012-12-14 23:30:03 -0500

    division prgm working

--- pset1/division.c
+++ pset1/division.c
@@ -9,27 +9,39 @@
 */
 
 //function prototypes
-float divide(int numerator, int denominator);
+float divide(float numerator, float denominator);
 
 #include <stdio.h>
 #include <cs50.h>
 
 int main(void)
 {
-
-}
-
-float divide(int numerator, int denominator)
-{
+    // make vars bad values initially so user will be asked for new ones
+    float numerator = 0;
+    float denominator = 0;
     // if user gives us bad input
-    while(numerator<=0 && denominator>0)
+    while(numerator<=0)
     {
     printf("non-negative numerator: ");
-    numerator = GetInt();
-    printf("positive denominator: ");   
-    denominator = GetInt();                                    
+    numerator = GetFloat();                                 
     }
+    while(denominator<=0)
+    {
+    printf("positive denominator: ");
+    denominator = GetFloat();
+    }
+    // user has given good input
+    float quotient = divide(numerator, denominator);
+    string percent_sign = "%";
+    printf("%.2f%s\n", quotient, percent_sign);
+}
+
+// divide two positive integers and return a percentage
+float divide(float numerator, float denominator)
+{
     // when user gives us good input
     float quotient = (numerator/denominator);
-    return quotient;
+    float percentage = quotient*100;
+    // note that the return value won't have a percent symbol or any formatting 
+    return percentage;
 }
