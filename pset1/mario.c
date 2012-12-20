c1b8f5b4aa2a7cfed254fad4078acf1b8e234f54
John Harvard <jharvard@appliance.localdomain>
2012-12-20 17:39:41 -0500

    added mario.c and template.c

---
+++ pset1/mario.c
@@ -0,0 +1,18 @@
+/*************************
+**************************
+* mario.c
+*
+* Computer Science 50x
+* Evan Sendra
+*
+* Builds a staircase of "#" blocks analagous to 
+* those in the old Mario video games.
+**************************
+*************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+}
