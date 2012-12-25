a4ddcbfc6bba47372cabff5ba3c3e529b3cd4080
John Harvard <jharvard@appliance.localdomain>
2012-12-25 08:24:56 -0500

    mario.c is almost working properly...just a few fixes at the bottom of the pyramid of blocks to go

--- pset1/mario.c
+++ pset1/mario.c
@@ -5,14 +5,42 @@
 * Computer Science 50x
 * Evan Sendra
 *
-* Builds a staircase of "#" blocks analagous to 
-* those in the old Mario video games.
+* builds a half pyramid of "#" blocks
+* as seen in the old mario video games
 **************************
 *************************/
 
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
 
+int numRows = 0;
+
+while(numRows <= 0 || numRows >= 23)
+{
+    //numRows must be an int between 1 and 23
+    printf("Height: ");
+    numRows = GetInt();
 }
+
+//this loop tracks vertical iterations 
+for(int i=1; i<=numRows; i++)
+{
+    //this loop tracks horizontal space printing
+    for(int j=1; j<(numRows-i); j++)
+        printf(" ");
+    //this loop tracks horizontal block printing
+    for(int k=0; k<(i+1); k++)
+    {
+        if(k==i)
+            printf("#\n");
+        else
+            printf("#");
+    }
+}
+//all is well
+return 0;
+
+} //end main
