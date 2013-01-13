fdd152144af6653d8588d5fc6fd65a4dfbeabb31
Evan Sendra (dorm room) <evan@evansendra.com>
2013-01-12 22:35:15 -0500

    saved files to make pull work

---
+++ pset2/template.c
@@ -0,0 +1,17 @@
+/*************************
+**************************
+* {name_of_program}.c
+*
+* Computer Science 50x
+* Evan Sendra
+*
+* {description_of_what_program_does}
+**************************
+*************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+}
