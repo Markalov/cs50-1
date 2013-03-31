2ecb084e660994f443b73a56d1f9e2b3cce77c20
John Harvard <jharvard@appliance.localdomain>
2013-03-31 12:05:05 -0400

    added a hello world prgm from new CS50 appliance on HP laptop

---
+++ helloWorld.c
@@ -0,0 +1,22 @@
+/*************************
+**************************
+* helloWorld.c
+*
+* Computer Science 50x
+* Evan Sendra
+*
+* performs a simple hello world
+* print to the screen to demonstrate
+* how C programs can be compiled and 
+* made in the cs50 appliance
+**************************
+*************************/
+
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string message = "Hello world!";
+    printf("%s\n", message);
+}
