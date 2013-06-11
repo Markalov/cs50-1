0957a8349ba99d6da2e0648d16dc9e6c75b853b5
Evan Sendra (ptel) <evan@evansendra.com>
2013-06-11 10:14:10 -0400

    modifying scramble

--- week3/pset3/scramble.c
+++ week3/pset3/scramble.c
@@ -9,7 +9,7 @@
  *
  * where # is an optional grid number.
  ***************************************************************************/
- 
+
 #include <cs50.h>
 #include <ctype.h>
 #include <libgen.h>
