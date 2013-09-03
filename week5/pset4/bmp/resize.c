624bed03c232f8420c34b9f1127ad005626f3b89
John Harvard <jharvard@appliance.localdomain>
2013-09-03 19:37:47 -0400

    finished resize in pset 4

--- week5/pset4/bmp/resize.c
+++ week5/pset4/bmp/resize.c
@@ -9,21 +9,43 @@
        
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 #include "bmp.h"
 
 int main(int argc, char* argv[])
 {
     // ensure proper usage
-    if (argc != 3)
+    if (argc != 4)
     {
-        printf("Usage: whodunit cluefile verdictfile\n");
+        printf("Usage: resize n infile outfile\n");
         return 1;
     }
 
+    // check that n is an integer 
+    for (int i=0, l=strlen(argv[1]); i<l; i++)
+    {
+        if (!isdigit(argv[1][i]))
+        {
+            printf("n must be an integer\n");
+            return 2;
+        }
+    }
+
+    // remember n
+    int n = atoi(argv[1]);
+
+    // check that n is in [1,100]
+    if (n < 1 || n > 100)
+    {
+        printf("n must be an integer between [1,100]\n");
+        return 2;
+    }
+
     // remember filenames
-    char* infile = argv[1];
-    char* outfile = argv[2];
+    char* infile = argv[2];
+    char* outfile = argv[3];
 
     // open input file 
     FILE* inptr = fopen(infile, "r");
@@ -60,50 +82,58 @@ int main(int argc, char* argv[])
         return 4;
     }
 
+    // modify data of headers necessary to resize the file
+    int origWidth = bi.biWidth;
+    // determine original padding for scanlines
+    int origPadding =  (4 - (origWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    // scale width and height by factor of n
+    bi.biWidth *= n;
+    bi.biHeight *= n;
+
+    // determine padding for scanlines
+    int padding =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+    // biSizeImage = width * height * 3 bytes pp + (padding per line)
+    bi.biSizeImage = (bi.biWidth * abs(bi.biHeight) * 3) + padding*abs(bi.biHeight);
+    // change the file size to reflect the new file size
+    bf.bfSize = bi.biSizeImage + bf.bfOffBits;
+
+    
     // write outfile's BITMAPFILEHEADER
     fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, outptr);
 
     // write outfile's BITMAPINFOHEADER
     fwrite(&bi, sizeof(BITMAPINFOHEADER), 1, outptr);
-
-    // determine padding for scanlines
-    int padding =  (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
-
+    
     // iterate over infile's scanlines
     for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
     {
         // iterate over pixels in scanline
-        for (int j = 0; j < bi.biWidth; j++)
+        for (int j = 0; j < origWidth; j++)
         {
             // temporary storage
             RGBTRIPLE triple;
-
+            
             // read RGB triple from infile
             fread(&triple, sizeof(RGBTRIPLE), 1, inptr);
 
-            // if pixel is red, make white
-            if(triple.rgbtRed == 0xff && triple.rgbtBlue == 0x00 &&
-            triple.rgbtGreen == 0x00)
-            {
-                triple.rgbtGreen = 0xff;
-                triple.rgbtBlue = 0xff;
-            }
-
-            // make every pixel darker
-            triple.rgbtRed -= 0x99;
-            triple.rgbtGreen -= 0x99;
-            triple.rgbtBlue -= 0x99;
-
-            // write RGB triple to outfile
-            fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
+            // write RGB triple to outfile n times horizontally
+            for(int k = 0; k < n; k++)
+                fwrite(&triple, sizeof(RGBTRIPLE), 1, outptr);
         }
 
         // skip over padding, if any
-        fseek(inptr, padding, SEEK_CUR);
-
+        fseek(inptr, origPadding, SEEK_CUR);
+        
         // then add it back (to demonstrate how)
         for (int k = 0; k < padding; k++)
             fputc(0x00, outptr);
+
+        // if i % n is 0, write a new row, else repeat the last row
+        long offset = origWidth*sizeof(RGBTRIPLE) + origPadding;
+        if((i+1) % n != 0)
+            fseek(inptr, -offset, SEEK_CUR);
     }
 
     // close infile
--- week5/pset4/jpg/recover.c
+++ week5/pset4/jpg/recover.c
@@ -1,8 +1,17 @@
 /****************************************************************************
- * recover.c
- *
- * Computer Science 50
- * Problem Set 4
- *
- * Recovers JPEGs from a forensic image.
- ***************************************************************************/
+* recover.c
+*
+* Computer Science 50
+* Problem Set 4
+*
+* Recovers JPEGs from a forensic image.
+***************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cs50.h>
+
+int main(void)
+{
+
+}
